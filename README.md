29fc6ead14b226d920cfe55b6a7a808dbdec4f89
Bert Spaan <bert.spaan@gmail.com>
2025-11-11 12:06:43 +0100

    store annotations.json, added github pages script, single-level info.jsons

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # sprite-test
 
+We'll use this Georeference Annotation as an example:
+
+https://raw.githubusercontent.com/tu-delft-heritage/watertijdreis-data/refs/heads/main/content/annotations/01-1874-389916-georef.json
+
 Run:
 
 ```
 pnpm install
-node index.ts
-mkdir output
+node index.ts "https://raw.githubusercontent.com/tu-delft-heritage/watertijdreis-data/refs/heads/main/content/annotations/01-1874-389916-georef.json" 128,256,512
 ```
--- index.ts
+++ index.ts
@@ -12,17 +12,30 @@ import {
   type GeoreferencedMap
 } from '@allmaps/annotation'
 
-const spriteWidth = 512 // Width of sprite images, in pixels
-const annotationUrl =
-  'https://raw.githubusercontent.com/tu-delft-heritage/watertijdreis-data/refs/heads/main/content/annotations/01-1874-389916-georef.json'
+// Width of sprite images, in pixels
+const annotationUrl = process.argv[2]
+const spriteWidths = (process.argv[3] || '128')
+  .split(',')
+  .map(Number)
+  .filter((w) => !isNaN(w))
 
 function getSpriteUrl(imageId: string, width: number) {
   return `${imageId}/full/${width},/0/default.jpg`
 }
 
-const annotations = await fetch(annotationUrl).then((response) =>
-  response.json()
-)
+const annotationsId = await generateId(annotationUrl)
+const outputDir = path.join('./output/', annotationsId)
+
+await mkdirp(outputDir)
+
+let annotations
+if (!fs.existsSync(path.join(outputDir, 'annotations.json'))) {
+  annotations = await fetch(annotationUrl).then((response) => response.json())
+} else {
+  annotations = JSON.parse(
+    fs.readFileSync(path.join(outputDir, 'annotations.json'), 'utf-8')
+  )
+}
 
 const maps = parseAnnotation(annotations)
 
@@ -38,82 +51,94 @@ type Box = {
 
 const boxes: Box[] = []
 
-for (const map of maps) {
-  const imageId = map.resource.id
-  const allmapsImageId = await generateId(imageId)
+for (const spriteWidth of spriteWidths) {
+  for (const map of maps) {
+    const imageId = map.resource.id
+    const allmapsImageId = await generateId(imageId)
 
-  const cacheDir = `./cache/${spriteWidth}`
-  await mkdirp(cacheDir)
-  const cacheFilename = `${cacheDir}/${allmapsImageId}.jpg`
+    const cacheDir = path.join('./cache/', annotationsId, String(spriteWidth))
+    await mkdirp(cacheDir)
+    const cacheFilename = path.join(cacheDir, `${allmapsImageId}.jpg`)
 
-  if (fs.existsSync(cacheFilename)) {
-    // File already cached, do nothing
-  } else {
-    const spriteUrl = getSpriteUrl(map.resource.id, spriteWidth)
-    console.log(`Downloading image ${spriteUrl}...`)
+    if (fs.existsSync(cacheFilename)) {
+      // File already cached, do nothing
+    } else {
+      const spriteUrl = getSpriteUrl(map.resource.id, spriteWidth)
+      console.log(`Downloading image ${spriteUrl}...`)
 
-    const imageBuffer = await fetch(spriteUrl).then((res) => res.arrayBuffer())
-    fs.writeFileSync(cacheFilename, Buffer.from(imageBuffer))
+      const imageBuffer = await fetch(spriteUrl).then((res) =>
+        res.arrayBuffer()
+      )
+      fs.writeFileSync(cacheFilename, Buffer.from(imageBuffer))
+    }
+
+    const sprite = fs.readFileSync(cacheFilename)
+    const imageMetadata = await sharp(sprite).metadata()
+
+    boxes.push({
+      map,
+      sprite,
+      w: imageMetadata.width,
+      h: imageMetadata.height,
+      scale: imageMetadata.width / (map.resource.width || 1),
+      x: 0,
+      y: 0
+    })
   }
 
-  const sprite = fs.readFileSync(cacheFilename)
-  const imageMetadata = await sharp(sprite).metadata()
+  const { w: width, h: height } = potpack(boxes)
 
-  boxes.push({
-    map,
-    sprite,
-    w: imageMetadata.width,
-    h: imageMetadata.height,
-    scale: imageMetadata.width / (map.resource.width || 1),
-    x: 0,
-    y: 0
-  })
+  const spritesId = 'http://127.0.0.1:8080'
+
+  const sprites = await sharp({
+    create: {
+      width,
+      height,
+      channels: 3,
+      background: { r: 255, g: 255, b: 255 }
+    }
+  }).composite(
+    boxes.map((box) => ({ input: box.sprite, left: box.x, top: box.y }))
+  )
+
+  await mkdirp(path.join(outputDir, String(spriteWidth)))
+
+  await sprites.toFile(
+    path.join(outputDir, String(spriteWidth), 'thumbnail-sprites.jpg')
+  )
+  await sprites
+    .tile({ size: 1024, layout: 'iiif3', id: spritesId, depth: 'one' })
+    .toFile(path.join(outputDir, String(spriteWidth), 'iiif'))
+
+  const newMaps = boxes.map((box) => ({
+    '@context': 'https://schemas.allmaps.org/map/2/context.json',
+    type: 'GeoreferencedMap',
+    resource: {
+      id: path.join(spritesId, 'iiif'),
+      width,
+      height,
+      type: 'ImageService3'
+    },
+    gcps: box.map.gcps.map((gcp) => ({
+      resource: [
+        box.x + gcp.resource[0] * box.scale,
+        box.y + gcp.resource[1] * box.scale
+      ],
+      geo: gcp.geo
+    })),
+    resourceMask: box.map.resourceMask.map(([x, y]) => [
+      box.x + x * box.scale,
+      box.y + y * box.scale
+    ]),
+    transformation: box.map.transformation
+  }))
+
+  fs.writeFileSync(
+    path.join(
+      outputDir,
+      String(spriteWidth),
+      'thumbnail-sprites-annotation.json'
+    ),
+    JSON.stringify(generateAnnotation(newMaps), null, 2)
+  )
 }
-
-const { w: width, h: height } = potpack(boxes)
-
-const spritesId = 'http://127.0.0.1:8080'
-
-const sprites = await sharp({
-  create: {
-    width,
-    height,
-    channels: 3,
-    background: { r: 255, g: 255, b: 255 }
-  }
-}).composite(
-  boxes.map((box) => ({ input: box.sprite, left: box.x, top: box.y }))
-)
-
-await sprites.toFile('./output/sprites.jpg')
-await sprites
-  .tile({ size: 1024, layout: 'iiif3', id: spritesId })
-  .toFile('./output/iiif')
-
-const newMaps = boxes.map((box) => ({
-  '@context': 'https://schemas.allmaps.org/map/2/context.json',
-  type: 'GeoreferencedMap',
-  resource: {
-    id: path.join(spritesId, 'iiif'),
-    width,
-    height,
-    type: 'ImageService3'
-  },
-  gcps: box.map.gcps.map((gcp) => ({
-    resource: [
-      box.x + gcp.resource[0] * box.scale,
-      box.y + gcp.resource[1] * box.scale
-    ],
-    geo: gcp.geo
-  })),
-  resourceMask: box.map.resourceMask.map(([x, y]) => [
-    box.x + x * box.scale,
-    box.y + y * box.scale
-  ]),
-  transformation: box.map.transformation
-}))
-
-fs.writeFileSync(
-  './output/sprites.json',
-  JSON.stringify(generateAnnotation(newMaps), null, 2)
-)
