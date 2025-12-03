e6e60843f18be14b047ba259270ea41b6f9afcef
Jules Schoonman <jules.schoonman@gmail.com>
2025-12-03 11:49:59 +0100

    new sprite

--- run.sh
+++ run.sh
@@ -3,3 +3,4 @@
 node index.ts "https://raw.githubusercontent.com/tu-delft-heritage/watertijdreis-data/refs/heads/main/content/annotations/01-1874-389916-georef.json" 0.5,1,2
 node index.ts "https://sammeltassen.nl/iiif-manifests/allmaps/top25-1.json" 0.25,0.5,1
 node index.ts "https://sammeltassen.nl/iiif-manifests/allmaps/bonnebladen-dans-1.json" 0.25,0.5,1
+node index.ts "https://tu-delft-heritage.github.io/watertijdreis-data/annotations-regular.json" 0.5,1
